22bd8bfa3984be2637c2c3ab02d8ea202825394c
Joseph Sungpil Choi <chousemath@gmail.com>
2017-05-12 23:52:51 +0900

    researched types a little bit

--- practice_1/app.ts
+++ practice_1/app.ts
@@ -1,1 +1,17 @@
-console.log('hello world!')
+// string type
+let myName = 'Max'
+
+// Type '55' is not assignable to type 'string'.
+// reassigning to number throws err
+// in regular JS, we can do this
+// myName = 55
+
+// number type
+let myAge = 26.5
+// Type '"potato"' is not assignable to type 'number'.
+// myAge = 'potato'
+
+// boolean type
+let hasHobbies = true
+// Type '1' is not assignable to type 'boolean'.
+// hasHobbies = 1
