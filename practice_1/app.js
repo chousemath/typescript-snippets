1e0b7363ec6d7da5405e46e874ff56a868f038a3
Joseph Sungpil Choi <chousemath@gmail.com>
2017-05-13 00:14:28 +0900

    added a few more notes on types

--- practice_1/app.js
+++ practice_1/app.js
@@ -1,2 +1,57 @@
 "use strict";
-console.log('hello world!');
+// string type
+var myName = 'Max';
+// Type '55' is not assignable to type 'string'.
+// reassigning to number throws err
+// in regular JS, we can do this
+// myName = 55
+var hisName;
+hisName = 'John';
+// number type
+var myAge = 26.5;
+// Type '"potato"' is not assignable to type 'number'.
+// myAge = 'potato'
+// boolean type
+var hasHobbies = true;
+// Type '1' is not assignable to type 'boolean'.
+// hasHobbies = 1
+var iAmAwesome;
+iAmAwesome = false;
+// explicity assign types
+var myKoreanAge;
+myKoreanAge = 28;
+// Type '"28"' is not assignable to type 'number'.
+// myKoreanAge = '28'
+// array type
+var foods = ['pizza', 'pineapple'];
+alert(foods[1]);
+// Type 'number[]' is not assignable to type 'string[]'.
+// Type 'number' is not assignable to type 'string'.
+// TypeScript is expecting an array of strings
+// foods = [1, 2]
+// the `any` type allows for relaxed reassignment
+var sizes;
+sizes = ['a', 'b'];
+sizes = [1, 2];
+// tuple type
+// order is important in a tuple type
+var identifier = ['Joseph', 26];
+var cityState = ['Baltimore', 'Maryland'];
+var bankDeposit = [999, 'AJF26KAJASDF'];
+// enum type
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor); // outputs: 1
+// you can also explicity declare values for each enum
+var Car;
+(function (Car) {
+    Car[Car["Toyota"] = 111] = "Toyota";
+    Car[Car["Hyundai"] = 222] = "Hyundai";
+    Car[Car["Mitsubishi"] = 333] = "Mitsubishi";
+})(Car || (Car = {}));
+console.log(Car.Toyota + Car.Hyundai + Car.Mitsubishi); // outputs: 777
--- practice_1/app.ts
+++ practice_1/app.ts
@@ -1,10 +1,11 @@
 // string type
 let myName = 'Max'
-
 // Type '55' is not assignable to type 'string'.
 // reassigning to number throws err
 // in regular JS, we can do this
 // myName = 55
+let hisName: string
+hisName = 'John'
 
 // number type
 let myAge = 26.5
@@ -15,3 +16,50 @@ let myAge = 26.5
 let hasHobbies = true
 // Type '1' is not assignable to type 'boolean'.
 // hasHobbies = 1
+let iAmAwesome: boolean
+iAmAwesome = false
+
+// explicity assign types
+let myKoreanAge: number
+myKoreanAge = 28
+// Type '"28"' is not assignable to type 'number'.
+// myKoreanAge = '28'
+
+// array type
+let foods = ['pizza', 'pineapple']
+alert(foods[1])
+// Type 'number[]' is not assignable to type 'string[]'.
+// Type 'number' is not assignable to type 'string'.
+// TypeScript is expecting an array of strings
+// foods = [1, 2]
+
+// the `any` type allows for relaxed reassignment
+let sizes: any
+sizes = ['a', 'b']
+sizes = [1, 2]
+
+// tuple type
+// order is important in a tuple type
+let identifier: [string, number] = ['Joseph', 26]
+let cityState: [string, string] = ['Baltimore', 'Maryland']
+let bankDeposit: [number, string] = [999, 'AJF26KAJASDF']
+
+// enum type
+enum Color { Red, Green, Blue }
+let myColor: Color = Color.Green
+console.log(myColor) // outputs: 1
+
+// you can also explicity declare values for each enum
+enum Car {
+  Toyota     = 111,
+  Hyundai    = 222,
+  Mitsubishi = 333
+}
+console.log(Car.Toyota + Car.Hyundai + Car.Mitsubishi) // outputs: 666
+
+// any type
+// this is basically a regular, JavaSCript, dynamic type
+let myVar: any
+myVar = 'dog'
+myVar = 123
+myVar = false
