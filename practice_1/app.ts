cc9e5b7d4d91158f1632606d5dcd9a5bb01514dd
Joseph Sungpil Choi <chousemath@gmail.com>
2017-05-15 04:38:11 +0900

    just removed some spaces

--- practice_1/app.ts
+++ practice_1/app.ts
@@ -1,4 +1,4 @@
-// code challenge 1
+// coding challenge 1
 type bankAccount = {money: number, deposit: (value: number) => void}
 let bankAccount: bankAccount = {
   money: 2000,
@@ -6,13 +6,10 @@ let bankAccount: bankAccount = {
     this.money += value
   }
 }
-
 let myself: {name: string, bankAccount: bankAccount, hobbies: string[]} = {
   name: 'Max',
   bankAccount: bankAccount,
   hobbies : ['Sports', 'Cooking']
 }
-
 myself.bankAccount.deposit(3000)
-
 console.log(myself)
